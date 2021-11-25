f97592935b7d5dda52652f6c4497e20e19528216
QZou777 <42842130+QZou777@users.noreply.github.com>
2021-11-24 21:19:30 -0600

    Create Higher Than 75 Marks.sql

---
+++ Higher Than 75 Marks.sql
@@ -0,0 +1,6 @@
+-- Question 1: Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+-- HackerRank Link: https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- Solution:
+Select Name from STUDENTS
+where Marks>75
+order by right(Name, 3) asc, ID asc
