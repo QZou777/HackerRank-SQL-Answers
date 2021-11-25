5cc9b52448f1631cbb2b9fed0d0b277834450eb1
QZou777 <42842130+QZou777@users.noreply.github.com>
2021-11-24 21:22:27 -0600

    Create Employee Names.sql

---
+++ Employee Names.sql
@@ -0,0 +1,4 @@
+-- Question 1:  Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+-- HackerRank Link: https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- Solution:
+select name from Employee order by name
