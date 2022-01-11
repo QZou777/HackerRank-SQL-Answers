641c4019899c0b6a06a8828b28235bb1633b9e2a
QZou777 <42842130+QZou777@users.noreply.github.com>
2022-01-11 12:23:17 -0600

    Create Weather Observation Station 4.sql

---
+++ Weather Observation Station 4.sql
@@ -0,0 +1,6 @@
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
+-- Question:
+-- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. 
+
+select count(CITY)-count(DISTINCT(CITY))
+from STATION
